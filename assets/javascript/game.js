24dbb228655a81c8530a1cf556ee983cd98fb288
Linh Nguyen <linh0404@hotmail.com>
2019-07-03 23:05:24 +1000

    added if functions detailing actions for each scenario

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y", "Z"]
 
 // set all values to zero
-var win = 0;
+var wins = 0;
 var losses = 0;
 var guessLeft = 9;
 var guesses = [];
@@ -10,7 +10,7 @@ var guesses = [];
 var letsplay = confirm ("Are you eligible for enrolment into Hogwarts? Lets play to find out.")
 
 if (letsplay) {
-    prompt ("What's your name?");
+    var userName = prompt ("What's your name?");
     alert("What letter am I thinking of? Guess correctly to be accepted");
 }
 
@@ -18,15 +18,35 @@ else {
     alert("Aww, just a squib");
 }
 
+// random letter to act as the computers choice
+var compGuess = letters[Math.floor(Math.random() * letters.length)];
+
 // an event function to note when a choice has been made
 var userGuess = document.onkeyup;
 
 document.onkeyup = function (event) {
     userGuess = event.key;
+    console.log(compGuess)
+    guesses.push(userGuess);
     alert("You chose " + userGuess.toUpperCase())
 }
 
-// random letter to act as the computers choice
-var compChoice = letters[Math.floor(Math.random() * letters.length)];
+//if functions to the actions for each scenario
+if (compGuess == userGuess) {
+    win++;
+    guessLeft = 9;
+    guesses = []
+}
+
+if (compGuess != userGuess) {
+    guessLeft--;
+}
+
+if (guessLeft == 0) {
+    losses++;
+    guessLeft = 9;
+    guesses = []
+}
+
 
 
