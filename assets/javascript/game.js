bc9640d3361424f351dabd0d2b67a8921f3d1ce2
Linh Nguyen <linh0404@hotmail.com>
2019-07-03 22:36:22 +1000

    alert when a guess is made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,18 +7,24 @@ var losses = 0;
 var guessLeft = 9;
 var guesses = [];
 
-var letsplay = confirm ("Are you eligible for enrolment into Hogwarts?")
+var letsplay = confirm ("Are you eligible for enrolment into Hogwarts? Lets play to find out.")
 
 if (letsplay) {
-    alert("What letter am I thinking of? Guess correctly to find out")
+    prompt ("What's your name?");
+    alert("What letter am I thinking of? Guess correctly to be accepted");
 }
 
 else {
-    alert("Aww, just a squib")
+    alert("Aww, just a squib");
 }
 
 // an event function to note when a choice has been made
 var userGuess = document.onkeyup;
+
+document.onkeyup = function (event) {
+    userGuess = event.key;
+    alert("You chose " + userGuess.toUpperCase())
+}
 
 // random letter to act as the computers choice
 var compChoice = letters[Math.floor(Math.random() * letters.length)];
