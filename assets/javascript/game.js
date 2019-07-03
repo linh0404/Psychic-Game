fa48bb27ef200a6bcd33ff12564ef79ce2e6eefd
Linh Nguyen <linh0404@hotmail.com>
2019-07-03 22:23:50 +1000

    added a confirm at the start for the user

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,26 @@
 // setting available choices for the computer
-var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","q","r","s","t","u","v","w","x","y","z"]
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y", "Z"]
 
 // set all values to zero
 var win = 0;
 var losses = 0;
 var guessLeft = 9;
 var guesses = [];
+
+var letsplay = confirm ("Are you eligible for enrolment into Hogwarts?")
+
+if (letsplay) {
+    alert("What letter am I thinking of? Guess correctly to find out")
+}
+
+else {
+    alert("Aww, just a squib")
+}
+
+// an event function to note when a choice has been made
 var userGuess = document.onkeyup;
+
+// random letter to act as the computers choice
+var compChoice = letters[Math.floor(Math.random() * letters.length)];
+
+
