c747a5457e2723a749d2e828fa7006241d3604c1
Linh Nguyen <linh0404@hotmail.com>
2019-07-03 21:56:06 +1000

    added variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// setting available choices for the computer
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","q","r","s","t","u","v","w","x","y","z"]
+
+// set all values to zero
+var win = 0;
+var losses = 0;
+var guessLeft = 9;
+var guesses = [];
+var userGuess = document.onkeyup;
