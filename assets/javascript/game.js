97f931d3a734ceae2992b32cd00b605071270dd1
Linh Nguyen <linh0404@hotmail.com>
2019-07-04 18:06:03 +1000

    fixed the compGuess so that it was one per round

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,52 +6,54 @@ var wins = 0;
 var losses = 0;
 var guessLeft = 9;
 var attempts = [];
-var compGuess = compGuess;
+var compGuess = "";
 
 var letsplay = confirm ("Are you eligible for enrolment into Hogwarts? Lets play to find out.")
+
+function reset() {
+    guessLeft = 9;
+    attempts = [];
+    //generate a random letter as the computer's choice
+    compGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log(compGuess);
+}
 
 if (letsplay) {
     var userName = prompt ("What's your name?");
     alert("What letter am I thinking of? Guess correctly to be accepted");
+    reset();    
 }
 
 else {
     alert("Aww, just a squib");
 }
 
-//generate a random letter as the computer's choice
-var  compGuess = letters[Math.floor(Math.random() * letters.length)];
-console.log(compGuess);
-
 // an event function to note when a choice has been made
     document.onkeyup = function (event) {
+        console.log(compGuess)
     var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
     console.log(userGuess);
-    //generate a random letter as the computer's choice
-    var  compGuess = letters[Math.floor(Math.random() * letters.length)];
-    console.log(compGuess);
 
 //if functions detailing actions for each scenario
 
         // scenario if user guesses correctly i.e. +1 to win, guesses do not decrement and attempt field clears
         if (userGuess === compGuess) {
             wins++;
-            guessLeft = 9;
-            attempts = [];
+            reset();
         }
 
         // scenario where guesses incorrectly i.e. pushes incorrect answer to attempts array and decrements no. of turns
         if (userGuess != compGuess) {
             attempts.push(userGuess);
             guessLeft--;
+           
         }
 
         // scenario once guesses left is 0 i.e. increase loss, clears attempts array and resets no. of guesses
         if (guessLeft === 0) {
             alert("Hmm, the sorting hat is still deciding...");
             losses++;
-            attempts = [];
-            guessLeft = 9;
+            reset();
         }
 
         // once losses are at 3, game over
@@ -68,13 +70,17 @@ console.log(compGuess);
             audio.play();
         }
 
+        //change the html so that it displays the results
         var html = 
             "<p> Wins: " + wins + "</p>" +
             "<p> Losses: " + losses + "</p>" +
             "<p> Guesses remaining: " + guessLeft + "</p>" +
             "<p> You chose: " + attempts + "</p>";
 
-        document.querySelector("#game").innerHTML = html;}
+        document.querySelector("#game").innerHTML = html;
+    
+    }
+
     
  
 
